6bdc7b24bfe3b66753495a4c7a1cd06fb96bb474
sudeepmi <sudeepm0599@gmail.com>
2023-12-15 14:25:29 +0545

    onboard page

---
+++ lib/onboard/pages/onboard.dart
@@ -0,0 +1,234 @@
+import 'package:flutter/material.dart';
+import 'package:onboarding/onboarding.dart';
+
+class OnboardPage extends StatefulWidget {
+  const OnboardPage({super.key});
+
+  @override
+  State<OnboardPage> createState() => _OnboardPageState();
+}
+
+class _OnboardPageState extends State<OnboardPage> {
+  late Material materialButton;
+  late int index;
+  final onboardingPagesList = [
+    PageModel(
+      widget: Column(
+        children: [
+          Expanded(
+            child: Image.asset(
+              "assets/images/1.png",
+              color: Colors.black,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: const Color.fromARGB(255, 255, 255, 255),
+            ),
+          )
+        ],
+      ),
+    ),
+    PageModel(
+      widget: DecoratedBox(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(
+            width: 0.0,
+            color: Colors.white,
+          ),
+        ),
+        child: SingleChildScrollView(
+          controller: ScrollController(),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 45.0,
+                  vertical: 90.0,
+                ),
+                child: Image.asset('assets/images/2.png'),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 45.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'CHANGE AND RISE',
+                    style: pageTitleStyle,
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 45.0, vertical: 10.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Give others access to any file or folders you choose',
+                    style: pageInfoStyle,
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+    PageModel(
+      widget: DecoratedBox(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(
+            width: 0.0,
+            color: Colors.white,
+          ),
+        ),
+        child: SingleChildScrollView(
+          controller: ScrollController(),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 45.0,
+                  vertical: 90.0,
+                ),
+                child: Image.asset('assets/images/3.png'),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 45.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'EASY ACCESS',
+                    style: pageTitleStyle,
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 45.0, vertical: 10.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Reach your files anytime from any devices anywhere',
+                    style: pageInfoStyle,
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    materialButton = _skipButton();
+    index = 0;
+  }
+
+  Material _skipButton({void Function(int)? setIndex}) {
+    return Material(
+      borderRadius: defaultSkipButtonBorderRadius,
+      borderOnForeground: true,
+      color: Colors.transparent,
+      child: InkWell(
+        borderRadius: defaultSkipButtonBorderRadius,
+        onTap: () {
+          if (setIndex != null) {
+            index = index + 1;
+            setIndex(index);
+          }
+        },
+        child: const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          child: Text(
+            'Next',
+            style: TextStyle(
+              color: Colors.red,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Material get _signupButton {
+    return Material(
+      borderRadius: const BorderRadius.all(Radius.circular(2)),
+      color: Colors.red,
+      child: InkWell(
+        onTap: () {},
+        child: const Padding(
+          padding: EdgeInsets.all(10),
+          child: Text(
+            'Sign up',
+            style: defaultProceedButtonTextStyle,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Onboarding(
+        pages: onboardingPagesList,
+        onPageChange: (int pageIndex) {
+          index = pageIndex;
+        },
+        startPageIndex: 0,
+        footerBuilder: (context, dragDistance, pagesLength, setIndex) {
+          return DecoratedBox(
+            decoration: const BoxDecoration(),
+            child: ColoredBox(
+                color: const Color.fromARGB(221, 255, 255, 255),
+                child: Container(
+                    // color: Colors.black,
+                    alignment: Alignment.center,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 40, horizontal: 10),
+                      child: Row(children: [
+                        SizedBox(
+                          width: 250,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 50),
+                            child: CustomIndicator(
+                              netDragPercent: dragDistance,
+                              pagesLength: pagesLength,
+                              indicator: Indicator(
+                                closedIndicator:
+                                    const ClosedIndicator(color: Colors.red),
+                                indicatorDesign: IndicatorDesign.polygon(
+                                  polygonDesign: PolygonDesign(
+                                      polygon: DesignType.polygon_circle,
+                                      polygonSpacer: 20.0,
+                                      polygonRadius: 5),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: index < onboardingPagesList.length - 1
+                              ? _skipButton(setIndex: setIndex)
+                              : _signupButton,
+                        )
+                      ]),
+                    ))),
+          );
+        },
+      ),
+    );
+  }
+}
